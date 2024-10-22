6035aa2eb33601da6ae8b0aac0467b5198f6fde9
mcpablo <pablomconturso@gmail.com>
2024-10-22 19:13:48 -0300

    wip

--- src/controllers/pets.controller.js
+++ src/controllers/pets.controller.js
@@ -20,7 +20,10 @@ export class PetsController {
   createPet = async (req, res, next) => {
     try {
       const { name, specie, birthDate } = req.body;
-      if (!name || !specie || !birthDate) return res.status(400).send({ status: "error", error: "Incomplete values" });
+      if (!name || !specie || !birthDate)
+        return res
+          .status(400)
+          .send({ status: "error", error: "Incomplete values" });
       const pet = PetDTO.getPetInputFrom({ name, specie, birthDate });
       const result = await this.petService.create(pet);
       res.status(201).json({ status: "success", payload: result });
@@ -50,4 +53,26 @@ export class PetsController {
     }
   };
 
+  createPetWithImage = async (req, res, next) => {
+    try {
+      const file = req.file;
+      const { name, specie, birthDate } = req.body;
+      if (!name || !specie || !birthDate)
+        return res
+          .status(400)
+          .send({ status: "error", error: "Incomplete values" });
+      console.log(file);
+      const pet = PetDTO.getPetInputFrom({
+        name,
+        specie,
+        birthDate,
+        image: `${__dirname}/../public/img/${file.filename}`,
+      });
+      console.log(pet);
+      const result = await petsService.create(pet);
+      res.send({ status: "success", payload: result });
+    } catch (error) {
+      next(error);
+    }
+  };
 };
--- src/mocks/pet.mock.js
+++ src/mocks/pet.mock.js
@@ -3,7 +3,10 @@ import { fakerES_MX as faker } from "@faker-js/faker";
 export const generateSinglePet = () => {
     const pet = {
         name: faker.person.firstName(),
-        birthDate: faker.date.birthdate({ mode: 'year', min: 2008, max: 2024 })
+        birthDate: faker.date.birthdate({ mode: 'year', min: 2008, max: 2024 }),
+        specie: faker.animal.type(),
+        adopted: false,
+        image: faker.image.avatar(),
     }
     return pet
 };
@@ -13,7 +16,10 @@ export const generatePetsMock = () => {
     for(let i=0;i < amount; i++) {
         const pet = {
             name: faker.person.firstName(),
-            birthDate: faker.date.birthdate({ mode: 'year', min: 2008, max: 2024 })
+            birthDate: faker.date.birthdate({ mode: 'year', min: 2008, max: 2024 }),
+            specie: faker.animal.type(),
+            adopted: false,
+            image: faker.image.avatar(),
         };
         pets.push(pet)
     };
